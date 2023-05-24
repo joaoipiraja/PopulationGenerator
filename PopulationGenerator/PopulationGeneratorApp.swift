1efcc1d8f7dad90744157cdfe3dc42c26fffa885
joaoipiraja <joaovictorpiraja@gmail.com>
2023-05-24 11:07:43 -0300

    Initial Commit

---
+++ PopulationGenerator/PopulationGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopulationGeneratorApp.swift
+//  PopulationGenerator
+//
+//  Created by João Victor Ipirajá de Alencar on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct PopulationGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
